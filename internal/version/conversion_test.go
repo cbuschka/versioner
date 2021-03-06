941b6aafb2badb31e3b2a6633bd7e85d0860cae8
conni <cbuschka@gmail.com>
2021-03-06 17:42:51 +0100

    Added test for tag "v3.0.0".

--- internal/version/conversion_test.go
+++ internal/version/conversion_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestFilterConvertAndSortAsc(t *testing.T) {
-	tags := []string{"invalid", "1.0.0", "0.0.1", "123", "999.8.1"}
+	tags := []string{"invalid", "1.0.0", "0.0.1", "v3.0", "123", "999.8.1"}
 
 	versions, removedTags, err := FilterConvertAndSortAsc(tags)
 	if err != nil {
@@ -16,9 +16,10 @@ func TestFilterConvertAndSortAsc(t *testing.T) {
 	assert.Equal(t, 1, len(removedTags))
 	assert.Equal(t, "invalid", removedTags[0])
 
-	assert.Equal(t, 4, len(versions))
+	assert.Equal(t, 5, len(versions))
 	assert.Equal(t, "0.0.1", versions[0].String())
 	assert.Equal(t, "1.0.0", versions[1].String())
-	assert.Equal(t, "123.0.0", versions[2].String())
-	assert.Equal(t, "999.8.1", versions[3].String())
+	assert.Equal(t, "3.0.0", versions[2].String())
+	assert.Equal(t, "123.0.0", versions[3].String())
+	assert.Equal(t, "999.8.1", versions[4].String())
 }
