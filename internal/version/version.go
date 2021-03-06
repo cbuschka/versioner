d5b7b37a666f6acd5e1eb2eb2a9ef21216022690
conni <cbuschka@gmail.com>
2021-03-06 20:17:33 +0100

    Added calculating next version.

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,9 @@
 package version
 
 import (
+	"fmt"
 	goversion "github.com/hashicorp/go-version"
+	"strings"
 )
 
 type Version struct {
@@ -23,4 +25,35 @@ func (version Version) String() string {
 
 func (version Version) Original() string {
 	return version.version.Original()
+}
+
+func (version Version) Next() *Version {
+	prefix := ""
+	if strings.HasPrefix(version.version.Original(), "v") {
+		prefix = "v"
+	}
+	segments := version.version.Segments()
+	if version.version.Prerelease() == "" {
+		segments[len(segments)-1] = segments[len(segments)-1]+1
+	}
+
+	return toVersion(prefix, segments)
+}
+
+func toVersion(prefix string, segments []int) *Version {
+	var buf strings.Builder
+	fmt.Fprintf(&buf, "%s", prefix)
+	for index, segment := range segments {
+		if index == 0 {
+			fmt.Fprintf(&buf, "%d", segment)
+		} else {
+			fmt.Fprintf(&buf, ".%d", segment)
+		}
+	}
+	version, err := ParseVersion(buf.String())
+	if err != nil {
+		panic(err)
+	}
+
+	return version
 }
--- internal/version/version_test.go
+++ internal/version/version_test.go
@@ -14,3 +14,27 @@ func TestParseVersion(t *testing.T) {
 	assert.Equal(t, "1.0.0", version.String())
 	assert.Equal(t, "v1", version.Original())
 }
+
+func TestNext(t *testing.T) {
+	version, err := ParseVersion("v1")
+	if err != nil {
+		t.Errorf("Failure.")
+	}
+
+	next := version.Next()
+
+	assert.Equal(t, "1.0.1", next.String())
+	assert.Equal(t, "v1.0.1", next.Original())
+}
+
+func TestNextFromPrerelease(t *testing.T) {
+	version, err := ParseVersion("v1-beta")
+	if err != nil {
+		t.Errorf("Failure.")
+	}
+
+	next := version.Next()
+
+	assert.Equal(t, "1.0.0", next.String())
+	assert.Equal(t, "v1.0.0", next.Original())
+}
