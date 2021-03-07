dc29a891fb5e7a5ca0de86aadcf0fb71ab9bd6da
conni <cbuschka@gmail.com>
2021-03-07 06:07:58 +0100

    Added long mode to version command.

--- internal/cli/version_command.go
+++ internal/cli/version_command.go
@@ -2,12 +2,25 @@ package cli
 
 import (
 	"github.com/cbuschka/versioner/internal/build"
+	"runtime"
 )
 
-type VersionCommandConfig struct {}
+type VersionCommandConfig struct {
+	Long bool
+}
 
 func (config *VersionCommandConfig) Run() error {
 	buildInfo := build.GetBuildInfo()
-	Print("%s", buildInfo.Version)
+	if !config.Long {
+		Print("%s", buildInfo.Version)
+	} else {
+	        Print(`versioner version is %s.
+Built from commitish %s, on %s for %s/%s.
+OS is %s, arch is %s.`,
+                buildInfo.Version,
+                buildInfo.Commitish, buildInfo.BuildTime, buildInfo.OS, buildInfo.Arch,
+                runtime.GOOS, runtime.GOARCH)
+	}
+
 	return nil
 }
